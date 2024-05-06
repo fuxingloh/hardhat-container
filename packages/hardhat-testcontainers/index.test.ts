6a1bfde5accd8979acd17d8a9bd6a591d8713a21
Fuxing Loh <git@fuxing.dev>
2024-05-06 22:12:26 +0800

    chore: minor refactor

--- packages/hardhat-testcontainers/index.test.ts
+++ packages/hardhat-testcontainers/index.test.ts
@@ -1,9 +1,9 @@
 import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
-import { createPublicClient, http, PublicClient } from 'viem';
+import { createPublicClient, http } from 'viem';
 import { hardhat } from 'viem/chains';
 import waitFor from 'wait-for-expect';
 
-import { HardhatContainer, StartedHardhatContainer } from './index';
+import { HardhatClient, HardhatContainer, StartedHardhatContainer } from './index';
 
 describe('default container', () => {
   let container: StartedHardhatContainer;
@@ -33,14 +33,9 @@ describe('default container', () => {
 
 describe('auto mining container 2000ms interval', () => {
   let container: StartedHardhatContainer;
-  let client: PublicClient;
 
   beforeAll(async () => {
     container = await new HardhatContainer().withMiningInterval(2000).start();
-    client = createPublicClient({
-      chain: hardhat,
-      transport: http(container.getHostRpcEndpoint()),
-    });
   });
 
   afterAll(async () => {
@@ -49,8 +44,36 @@ describe('auto mining container 2000ms interval', () => {
 
   it('should auto mine block', async () => {
     await waitFor(async () => {
-      const blockNumber = await client.getBlockNumber();
+      const blockNumber = await container.client.getBlockNumber();
       expect(blockNumber).toBeGreaterThan(1n);
+    });
+  });
+});
+
+describe('HardhatClient', () => {
+  let container: StartedHardhatContainer;
+  let client: HardhatClient;
+
+  beforeAll(async () => {
+    container = await new HardhatContainer().withMiningInterval(0).start();
+    client = container.client;
+  });
+
+  afterAll(async () => {
+    await container.stop();
+  });
+
+  it('should snapshot and revert', async () => {
+    await client.mine({ blocks: 1 });
+    const snapshotId = await client.snapshot();
+    await client.mine({ blocks: 1 });
+    await client.revert({ id: snapshotId });
+    await client.setCoinbase({ address: '0x0000000000000000000000000000000000000000' });
+    await client.mine({ blocks: 2 });
+
+    await waitFor(async () => {
+      const blockNumber = await client.getBlockNumber();
+      expect(blockNumber).toBeGreaterThanOrEqual(3n);
     });
   });
 });
