5ba78a8dfb847760c4c8c7941a9e725b0bc9ba64
Fuxing Loh <git@fuxing.dev>
2024-06-11 00:25:32 +0800

    chore: bump to chainfile v0.5.0

--- packages/chainfile-hardhat/localhost.test.ts
+++ packages/chainfile-hardhat/localhost.test.ts
@@ -1,26 +1,52 @@
 import { ChainfileTestcontainers } from '@chainfile/testcontainers';
-import { afterAll, beforeAll, expect, it } from '@jest/globals';
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
 
 import localhost from './localhost.json';
 
-let testcontainers: ChainfileTestcontainers;
+describe('default', () => {
+  const testcontainers = new ChainfileTestcontainers(localhost);
 
-beforeAll(async () => {
-  testcontainers = await ChainfileTestcontainers.start(localhost);
-});
-
-afterAll(async () => {
-  await testcontainers.stop();
-});
-
-it('should rpc(eth_blockNumber)', async () => {
-  const response = await testcontainers.get('hardhat').rpc({
-    method: 'eth_blockNumber',
+  beforeAll(async () => {
+    await testcontainers.start();
   });
 
-  expect(response.status).toStrictEqual(200);
+  afterAll(async () => {
+    await testcontainers.stop();
+  });
 
-  expect(await response.json()).toMatchObject({
-    result: '0x0',
+  it('should rpc(eth_blockNumber)', async () => {
+    const response = await testcontainers.get('hardhat').rpc({
+      method: 'eth_blockNumber',
+    });
+
+    expect(response.status).toStrictEqual(200);
+    expect(await response.json()).toMatchObject({
+      result: '0x0',
+    });
+  });
+});
+
+describe('v2.22.3', () => {
+  const testcontainers = new ChainfileTestcontainers(localhost, {
+    version: '2.22.3',
+  });
+
+  beforeAll(async () => {
+    await testcontainers.start();
+  });
+
+  afterAll(async () => {
+    await testcontainers.stop();
+  });
+
+  it('should rpc(eth_blockNumber)', async () => {
+    const response = await testcontainers.get('hardhat').rpc({
+      method: 'eth_blockNumber',
+    });
+
+    expect(response.status).toStrictEqual(200);
+    expect(await response.json()).toMatchObject({
+      result: '0x0',
+    });
   });
 });
